3cdea90fb65e9cc65e14087432ba36a8ccd2ab82
satyamodi <62868428+satyamodi@users.noreply.github.com>
2020-06-30 11:25:22 +0530

    Update ocs-prereq.sh

--- aws/scripts/ocs-prereq.sh
+++ aws/scripts/ocs-prereq.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #OCS Operator will install its components only on nodes labelled for OCS with the key
 OCS_NODES=`oc get nodes --show-labels | grep storage-node |cut -d' ' -f1`
 for ocsnode in ${OCS_NODES[@]}; do
