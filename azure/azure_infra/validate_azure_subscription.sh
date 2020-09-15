3bb1c615782244a6d5b1993c29239d4b9a127778
Gudrun Kaufmann <Gudrun.Kaufmann@de.ibm.com>
2020-09-15 15:38:52 +0200

    Update service principal creation and subscription validation

--- azure/azure_infra/validate_azure_subscription.sh
+++ azure/azure_infra/validate_azure_subscription.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# Validate your Azure subscription and check if enough roles are assigned to the principal name.
+# Validate your Azure subscription and check if enough roles are assigned to the service principal name.
 # To be executed after az login.
 
-read -p "Enter the first part of your user principal name (e.g. john.doe): " principal_name
+read -p "Enter your service principal name assigned by the 'az ad sp create-for-rbac' command (e.g. john.doe.SP): " service_principal_name
 
-echo Hello $principal_name!
-echo Your Azure Subscriptions are as follows...
+echo "Hello $service_principal_name!"
+echo "Your Azure subscriptions are as follows ..."
 az account list --all --output table
 
 read -p "Is the default subscription correct? (y/n): " answer
@@ -15,9 +15,9 @@ if [[ "$answer" = "y" ]]; then
   export output_filename=Roles_table_$(date -u +"%Y%m%d-%H%M%S").txt
   az role assignment list --output table  >> $output_filename
   echo "These assigned roles can be found in file $output_filename: "
-  grep $principal_name $output_filename
+  grep $service_principal_name $output_filename
 
 else
-  echo "Please set the correct subscription with az account set -s <desired_subscription_id> and start this script again!"
+  echo "Please set the correct subscription with 'az account set -s <desired_subscription_id>' and start this script again!"
 fi
 
