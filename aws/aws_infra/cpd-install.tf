1f4c27d6214a6e88b30463582fc332a6602bbab2
Satya Prakash Modi <Satya.Prakash.Modi@ibm.com>
2020-11-10 11:57:53 +0530

    Update cpd-install.tf

--- aws/aws_infra/cpd-install.tf
+++ aws/aws_infra/cpd-install.tf
@@ -448,7 +448,7 @@ resource "null_resource" "install_db2wh" {
     provisioner "remote-exec" {
         inline = [
             "export KUBECONFIG=/home/${var.admin-username}/${local.ocpdir}/auth/kubeconfig",
-            "cat > ${local.installerhome}/cpd-db2wh.yaml <<EOL\n${data.template_file.cpd-service.rendered}\nEOL",
+            "cat > ${local.installerhome}/cpd-db2wh.yaml <<EOL\n${data.template_file.cpd-service-db2wh.rendered}\nEOL",
             "sed -i -e s#SERVICE#db2wh#g ${local.installerhome}/cpd-db2wh.yaml",
             "sed -i -e s#STORAGECLASS#${lookup(var.cpd-storageclass,var.storage-type)}#g ${local.installerhome}/cpd-db2wh.yaml",
             "oc create -f ${local.installerhome}/cpd-db2wh.yaml -n ${var.cpd-namespace}",
