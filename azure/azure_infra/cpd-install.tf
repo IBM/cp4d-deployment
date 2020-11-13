40b6c91c79a286c3745b05057f7e2b909bd877e1
Pravesh Humane <pravesh@ibm.com>
2020-11-12 18:21:16 -0800

    changed dmc yaml filename

--- azure/azure_infra/cpd-install.tf
+++ azure/azure_infra/cpd-install.tf
@@ -471,10 +471,10 @@ resource "null_resource" "install_db2wh" {
     provisioner "remote-exec" {
         inline = [
             "export KUBECONFIG=/home/${var.admin-username}/${local.ocpdir}/auth/kubeconfig",
-            "cat > ${local.installerhome}/cpd-dmc.yaml <<EOL\n${data.template_file.cpd-service-no-override.rendered}\nEOL",
-            "sed -i -e s#SERVICE#dmc#g ${local.installerhome}/cpd-dmc.yaml",
-            "sed -i -e s#STORAGECLASS#${local.cp-storageclass}#g ${local.installerhome}/cpd-dmc.yaml",
-            "oc create -f ${local.installerhome}/cpd-dmc.yaml -n ${var.cpd-namespace}",
+            "cat > ${local.installerhome}/cpd-dmc-db2wh.yaml <<EOL\n${data.template_file.cpd-service-no-override.rendered}\nEOL",
+            "sed -i -e s#SERVICE#dmc#g ${local.installerhome}/cpd-dmc-db2wh.yaml",
+            "sed -i -e s#STORAGECLASS#${local.cp-storageclass}#g ${local.installerhome}/cpd-dmc-db2wh.yaml",
+            "oc create -f ${local.installerhome}/cpd-dmc-db2wh.yaml -n ${var.cpd-namespace}",
             "./wait-for-service-install.sh dmc ${var.cpd-namespace}",
             "cat > ${local.installerhome}/cpd-db2wh.yaml <<EOL\n${data.template_file.cpd-service-no-override.rendered}\nEOL",
             "sed -i -e s#SERVICE#db2wh#g ${local.installerhome}/cpd-db2wh.yaml",
@@ -515,10 +515,10 @@ resource "null_resource" "install_db2oltp" {
     provisioner "remote-exec" {
         inline = [
             "export KUBECONFIG=/home/${var.admin-username}/${local.ocpdir}/auth/kubeconfig",
-            "cat > ${local.installerhome}/cpd-dmc.yaml <<EOL\n${data.template_file.cpd-service-no-override.rendered}\nEOL",
-            "sed -i -e s#SERVICE#dmc#g ${local.installerhome}/cpd-dmc.yaml",
-            "sed -i -e s#STORAGECLASS#${local.cp-storageclass}#g ${local.installerhome}/cpd-dmc.yaml",
-            "oc create -f ${local.installerhome}/cpd-dmc.yaml -n ${var.cpd-namespace}",
+            "cat > ${local.installerhome}/cpd-dmc-db2oltp.yaml <<EOL\n${data.template_file.cpd-service-no-override.rendered}\nEOL",
+            "sed -i -e s#SERVICE#dmc#g ${local.installerhome}/cpd-dmc-db2oltp.yaml",
+            "sed -i -e s#STORAGECLASS#${local.cp-storageclass}#g ${local.installerhome}/cpd-dmc-db2oltp.yaml",
+            "oc create -f ${local.installerhome}/cpd-dmc-db2oltp.yaml -n ${var.cpd-namespace}",
             "./wait-for-service-install.sh dmc ${var.cpd-namespace}",
             "cat > ${local.installerhome}/cpd-db2oltp.yaml <<EOL\n${data.template_file.cpd-service-no-override.rendered}\nEOL",
             "sed -i -e s#SERVICE#db2oltp#g ${local.installerhome}/cpd-db2oltp.yaml",
