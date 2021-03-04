0f01743389e3fb625c28427a9bee1b442c84788a
femi <femi@ibm.com>
2021-03-04 11:30:35 -0800

    external registry

--- selfmanaged-openshift/aws/cpd_module/install-cpd-operator.sh
+++ selfmanaged-openshift/aws/cpd_module/install-cpd-operator.sh
@@ -2,9 +2,17 @@
 
 APIKEY=$1
 OPT_NAMESPACE=$2
+EXTERNAL_CPD_REGISTRY=$3
+EXTERNAL_CPD_USER=$4
 
+if [[ $EXTERNAL_CPD_REGISTRY == "" ]]; then
 export CPD_REGISTRY=cp.icr.io/cp/cpd
 export CPD_REGISTRY_USER=cp
+else
+export CPD_REGISTRY_USER=$EXTERNAL_CPD_USER
+export CPD_REGISTRY=$EXTERNAL_CPD_REGISTRY
+oc create secret generic external-cpd-registry --from-literal=url="$EXTERNAL_CPD_REGISTRY" --from-literal=user="$EXTERNAL_CPD_USER" --from-literal=key="${APIKEY}" -n $OPT_NAMESPACE
+fi
 export CPD_REGISTRY_PASSWORD=$APIKEY
 export NAMESPACE=$OPT_NAMESPACE
 
