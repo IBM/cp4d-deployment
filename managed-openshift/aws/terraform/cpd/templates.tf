d4ce8cc90c816db3aacc0e6968f61738f5f4699b
femi <femi@ibm.com>
2021-06-15 15:50:58 -0700

    bedrock init

--- managed-openshift/aws/terraform/cpd/templates.tf
+++ managed-openshift/aws/terraform/cpd/templates.tf
@@ -1,7 +1,7 @@
 locals {
-  crio_config_data = base64encode(file("cpd/config/crio.conf"))
-  limits_config_data = base64encode(file("cpd/config/limits.conf"))
-  sysctl_config_data = base64encode(file("cpd/config/sysctl.conf"))
+  crio_config_data = base64encode(file("config/crio.conf"))
+  limits_config_data = base64encode(file("config/limits.conf"))
+  sysctl_config_data = base64encode(file("config/sysctl.conf"))
   license = var.accept_cpd_license == "accept" ? true : false
   override = var.storage_option == "efs" ? "" : var.storage_option
   storage_class = lookup(var.cpd_storageclass, var.storage_option)
@@ -73,27 +73,277 @@ spec:
 EOF
 }
 
-data "template_file" "cpd_service" {
+data "template_file" "bedrock_setup" {
   template = <<EOF
-apiVersion: metaoperator.cpd.ibm.com/v1
-kind: CPDService
+---
+apiVersion: operator.openshift.io/v1alpha1
+kind: ImageContentSourcePolicy
 metadata:
-  name: SERVICE-cpdservice
-  labels:
-    app.kubernetes.io/instance: ibm-cp-data-operator-SERVICE-cpdservice
-    app.kubernetes.io/managed-by: ibm-cp-data-operator
-    app.kubernetes.io/name: ibm-cp-data-operator-SERVICE-cpdservice
+  name: bedrock-zen-edge-mirror
 spec:
-  serviceName: SERVICE
-  skipImageTransfer: false
-  version: "latest"
-  storageClass: ${local.storage_class}
-  overrideConfig: "${local.override}"
-  flags: ""
-  autoPatch: false
-  scale: ""
-  optionalModules: []
-  license:
-    accept: ${local.license}
+  repositoryDigestMirrors:
+  - mirrors:
+    - hyc-cp4d-team-bootstrap-2-docker-local.artifactory.swg-devops.com
+    - hyc-cloud-private-daily-docker-local.artifactory.swg-devops.com/ibmcom
+    source: quay.io/opencloudio
+  - mirrors:
+    - hyc-cp4d-team-bootstrap-docker-local.artifactory.swg-devops.com
+    - hyc-cp4d-team-bootstrap-2-docker-local.artifactory.swg-devops.com
+    source: icr.io/cpopen
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: opencloud-operators
+  namespace: openshift-marketplace
+spec:
+  displayName: IBMCS Test Operators
+  publisher: IBM
+  sourceType: grpc
+  image: hyc-cloud-private-daily-docker-local.artifactory.swg-devops.com/ibmcom/ibm-common-service-catalog:latest-validated 
+  # Alternatively use docker.io/ibmcom/ibm-common-service-catalog:latest for GA untested bedrock build
+  updateStrategy:
+    registryPoll:
+      interval: 45m
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  labels:
+    openshift.io/cluster-monitoring: "true"
+  name: ibm-common-services
+spec: {}
+---
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: common-service-operator-group
+  namespace: ibm-common-services
+spec:
+  targetNamespaces:
+    - ibm-common-services
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: ibm-common-service-operator
+  namespace: ibm-common-services
+spec:
+  channel: v3
+  installPlanApproval: Automatic
+  name: ibm-common-service-operator
+  source: opencloud-operators
+  sourceNamespace: openshift-marketplace
+EOF
+}
+
+data "template_file" "zen_setup" {
+  template = <<EOF
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  labels:
+    openshift.io/cluster-monitoring: "true"
+  name: zen
+spec: {}
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: ibm-zen-operator-catalog
+  namespace: openshift-marketplace
+spec:
+  sourceType: grpc
+  image: hyc-cp4d-team-bootstrap-2-docker-local.artifactory.swg-devops.com/ibm-zen-operator-catalog:1.1.0-amd64-305
+  imagePullPolicy: Always
+  displayName: Cloud Pak for Data
+  publisher: IBM
+---
+apiVersion: operator.ibm.com/v1alpha1
+kind: OperandRequest
+metadata:
+  name: zen-service
+  namespace: zen
+spec:
+  requests:
+    - operands:
+        - name: ibm-zen-operator
+        - name: ibm-cert-manager-operator
+      registry: common-service
+      registryNamespace: ibm-common-services
+EOF
+}
+
+data "template_file" "zen_service_lite" {
+  template = <<EOF
+apiVersion: zen.cpd.ibm.com/v1
+kind: ZenService
+metadata:
+  name: lite
+  namespace: zen
+spec:
+  csNamespace: ibm-common-services
+  iamIntegration: true
+  version: ${var.cpd_version}
+  storageClass: ${var.storage_option}
+  cloudpakfordata: true 
+  zenCoreMetaDbStorageClass: ${var.storage_option}
+  #cert_manager_enabled: false
+EOF
+}
+
+data "template_file" "operand_registry" {
+  template = <<EOF
+apiVersion: v1
+items:
+- apiVersion: operator.ibm.com/v1alpha1
+  kind: OperandRegistry
+  metadata:
+    annotations:
+      version: 3.8.0
+    creationTimestamp: null
+    generation: 1
+    name: common-service
+    namespace: ibm-common-services
+  spec:
+    operators:
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-licensing-operator
+      namespace: ibm-common-services
+      packageName: ibm-licensing-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-mongodb-operator
+      namespace: ibm-common-services
+      packageName: ibm-mongodb-operator-app
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-cert-manager-operator
+      namespace: ibm-common-services
+      packageName: ibm-cert-manager-operator
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-iam-operator
+      namespace: ibm-common-services
+      packageName: ibm-iam-operator
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-healthcheck-operator
+      namespace: ibm-common-services
+      packageName: ibm-healthcheck-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-commonui-operator
+      namespace: ibm-common-services
+      packageName: ibm-commonui-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-management-ingress-operator
+      namespace: ibm-common-services
+      packageName: ibm-management-ingress-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-ingress-nginx-operator
+      namespace: ibm-common-services
+      packageName: ibm-ingress-nginx-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-auditlogging-operator
+      namespace: ibm-common-services
+      packageName: ibm-auditlogging-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-platform-api-operator
+      namespace: ibm-common-services
+      packageName: ibm-platform-api-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-monitoring-exporters-operator
+      namespace: ibm-common-services
+      packageName: ibm-monitoring-exporters-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-monitoring-prometheusext-operator
+      namespace: ibm-common-services
+      packageName: ibm-monitoring-prometheusext-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-monitoring-grafana-operator
+      namespace: ibm-common-services
+      packageName: ibm-monitoring-grafana-operator-app
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v3
+      installPlanApproval: Automatic
+      name: ibm-events-operator
+      namespace: ibm-common-services
+      packageName: ibm-events-operator
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: stable
+      installPlanApproval: Automatic
+      name: redhat-marketplace-operator
+      namespace: openshift-redhat-marketplace
+      packageName: redhat-marketplace-operator
+      scope: public
+      sourceName: certified-operators
+      sourceNamespace: openshift-marketplace
+    - channel: stable-v1
+      installPlanApproval: Automatic
+      name: ibm-zen-operator
+      namespace: ibm-common-services
+      packageName: ibm-zen-operator-catalog
+      scope: public
+      sourceName: opencloud-operators
+      sourceNamespace: openshift-marketplace
+    - channel: v1.1
+      installPlanApproval: Automatic
+      name: ibm-db2u-operator
+      namespace: ibm-common-services
+      packageName: db2u-operator
+      scope: public
+kind: List
+metadata:
+  resourceVersion: ""
+  selfLink: ""
 EOF
 }
--- managed-openshift/aws/terraform/cpd/variables.tf
+++ managed-openshift/aws/terraform/cpd/variables.tf
@@ -21,11 +21,13 @@ variable "openshift_token" {
 
 variable "login_cmd" {
   type = string
+  default = ""
 }
 
 variable "installer_workspace" {
   type        = string
   description = "Folder find the installation files"
+  default = ""
 }
 
 variable "accept_cpd_license" {
@@ -52,96 +54,14 @@ variable "cpd_namespace" {
   default = "cpd-tenant"
 }
 
-variable "data_virtualization" {
-  default = "no"
-}
-
-variable "apache_spark" {
-  default = "no"
-}
-
-variable "watson_knowledge_catalog" {
-  default = "no"
-}
-
-variable "watson_studio_library" {
-  default = "no"
-}
-
-variable "watson_machine_learning" {
-  default = "no"
-}
-
-variable "watson_ai_openscale" {
-  default = "no"
-}
-
-variable "cognos_dashboard_embedded" {
-  default = "no"
-}
-
-variable "streams" {
-  default = "no"
-}
-
-variable "streams_flows" {
-  default = "no"
-}
-
-variable "datastage" {
-  default = "no"
-}
-
-variable "db2_warehouse" {
-  default = "no"
-}
-
-variable "db2_advanced_edition" {
-  default = "no"
-}
-
-variable "data_management_console" {
-  default = "no"
-}
-
-variable "datagate" {
-  default = "no"
-}
-
-variable "decision_optimization" {
-  default = "no"
-}
-
-variable "cognos_analytics" {
-  default = "no"
-}
-
-variable "spss_modeler" {
-  default = "no"
-}
-
-variable "db2_bigsql" {
-  default = "no"
-}
-
-variable "planning_analytics" {
-  default = "no"
-}
-
-variable "cloudctl_version" {
-  default = "v3.6.0"
-}
-
-variable "datacore_version" {
-  default = "1.3.3"
-}
-
 variable "vpc_id" {
   type = string
+  default = ""
 }
 
 variable "storage_option" {
   type = string
+  default = "portworx"
 }
 
 variable "cpd_storageclass" {
@@ -150,6 +70,20 @@ variable "cpd_storageclass" {
   default     = {
     "portworx"   = "portworx-shared-gp3"
     "ocs"        = "ocs-storagecluster-cephfs"
-    "efs"        = "aws-efs"
   }
+}
+
+variable "cpd_version" {
+  type = string
+  default = "4.0.0"
+}
+
+variable "artifactory_username" {
+  type = string
+  default = ""
+}
+
+variable "artifactory_apikey" {
+  type = string
+  default = ""
 }
