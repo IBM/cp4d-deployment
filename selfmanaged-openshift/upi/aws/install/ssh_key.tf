dbef94e9ccd2ff55f1b361faeeb6f89c0c5f5fe4
muthukba <muthukba@in.ibm.com>
2021-06-30 20:13:49 +0530

    Removing UPI changes

--- selfmanaged-openshift/upi/aws/install/ssh_key.tf
+++
@@ -1,4 +0,0 @@
-resource "tls_private_key" "installkey" {
-  algorithm   = "RSA"
-  rsa_bits = 4096
-}
--- selfmanaged-openshift/upi/aws/output.tf
+++
@@ -1,7 +0,0 @@
-/* output "private_ssh_key" {
-  value = module.installer.private_ssh_key
-} */
-
-/* output "public_ssh_key" {
-  value = module.installer.public_ssh_key
-} */
