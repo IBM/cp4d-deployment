eb54901bcea6d41b488ef4678a10efa26d4616d5
Pravesh Humane <pravesh@ibm.com>
2021-07-29 18:52:20 -0700

    change in API for volume attachment script and updated terraform versions

--- managed-openshift/ibmcloud/main.tf
+++ managed-openshift/ibmcloud/main.tf
@@ -1,7 +1,7 @@
 terraform {
-  required_version = "v0.12.29"
+  required_version = "v0.12.31"
   required_providers {
-    ibm = "1.20.1"
+    ibm = "1.28.0"
     kubernetes = "1.13.3"
     null = "~> 3.0"
   }
