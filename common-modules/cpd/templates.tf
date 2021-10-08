64818e99c67b41a01a0c8005cbfdf91b279b3ae2
Vamshidhar Cholleti <vcholle1@in.ibm.com>
2021-10-08 16:22:20 +0530

    Update templates.tf

--- common-modules/cpd/templates.tf
+++ common-modules/cpd/templates.tf
@@ -642,25 +642,6 @@ EOF
 }
 
 #DV
-data "template_file" "ibm_dmc_operator_catalog_source" {
-  template = <<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: CatalogSource
-metadata:
-  name: ibm-dmc-operator-catalog
-  namespace: openshift-marketplace
-spec:
-  displayName: "IBM DMC Operator Catalog"
-  publisher: IBM
-  sourceType: grpc
-  image: icr.io/cpopen/ibm-operator-catalog:latest
-  imagePullPolicy: IfNotPresent
-  updateStrategy:
-    registryPoll:
-      interval: 45m
-EOF
-}
-
 
 data "template_file" "dv_sub" {
   template = <<EOF
