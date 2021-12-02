fe55ca3a138a0daf27eff337ee721235d1998817
Vamshidhar Cholleti <vcholle1@in.ibm.com>
2021-12-02 23:49:36 +0530

    Update variables.tf

--- common-modules/cpd/variables.tf
+++ common-modules/cpd/variables.tf
@@ -222,7 +222,7 @@ variable "db2_warehouse" {
   type        = map(string)
   default = {
     enable   = "no"
-    version  = "4.0.3"
+    version  = "4.0.5"
     channel  = "v1.0"
   }
 }
@@ -231,7 +231,7 @@ variable "db2_oltp" {
   type        = map(string)
   default = {
     enable   = "no"
-    version  = "4.0.3"
+    version  = "4.0.5"
     channel  = "v1.0"
   }
 }
@@ -249,7 +249,7 @@ variable "data_management_console" {
   type        = map(string)
   default = {
     enable   = "no"
-    version  = "4.0.3"
+    version  = "3.1.6"
     channel  = "v1.0"
   }
 }
