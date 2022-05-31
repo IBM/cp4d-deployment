cc3932de9982e05fb03095e6db6d5cd0e7a958cd
vamshi cholleti <vamshicholleti@vamshis-MBP.alphionsee.in>
2022-05-31 23:07:28 +0530

    updated EFS + NFS for managed & Selfmanaged

--- managed-openshift/aws/terraform/efs/main.tf
+++ managed-openshift/aws/terraform/efs/main.tf
@@ -10,134 +10,137 @@ data "aws_vpc" "cpd_vpc" {
 
 #AWS EFS Setup
 
-resource "null_resource" "cpd_efs" { 
-    triggers = { 
-        login_cmd           =  var.login_cmd
-        openshift_username      = regex("username (.*) --password", "${var.login_cmd}")[0]
-        openshift_api        =  regex("login (.*) --username","${var.login_cmd}")[0]
-        openshift_password      = regex("--password (.*)","${var.login_cmd}")[0]
-        cluster_type        = "managed"
-   }
-    provisioner "local-exec" {
-        command = <<EOF
-              bash efs/setup-efs-nfs-provisioner.sh ${self.triggers.openshift_api} '${self.triggers.openshift_username}' '${self.triggers.openshift_password}'  
-    EOF
-   }
-}
+# resource "null_resource" "cpd_efs" { 
+#     triggers = { 
+#         login_cmd           =  var.login_cmd
+#         openshift_username      = regex("username (.*) --password", "${var.login_cmd}")[0]
+#         openshift_api        =  regex("login (.*) --username","${var.login_cmd}")[0]
+#         openshift_password      = regex("--password (.*)","${var.login_cmd}")[0]
+#         cluster_type        = "managed"
+#    }
+#     provisioner "local-exec" {
+#         command = <<EOF
+#               bash efs/setup-efs-nfs-provisioner.sh ${self.triggers.openshift_api} '${self.triggers.openshift_username}' '${self.triggers.openshift_password}'  
+#     EOF
+#    }
+# }
 # ${self.triggers.login_cmd} || oc login ${self.triggers.openshift_api} -u '${self.triggers.openshift_username}' -p '${self.triggers.openshift_password}' --insecure-skip-tls-verify=true || oc login --server='${self.triggers.openshift_api}' --token='${self.triggers.openshift_token}'
 
 
-# resource "aws_efs_file_system" "cpd_efs" {
-#    creation_token = "cpd_efs"
-#    performance_mode = "generalPurpose"
-#    throughput_mode = "provisioned"
-#    provisioned_throughput_in_mibps = "250"
-#    encrypted = "true"
-#    #lifecycle_policy {
-#    #   transition_to_ia = "AFTER_30_DAYS"
-#    #}
-#    tags = {
-#      Name = var.cluster_name
-#    }
-#  }
+resource "aws_efs_file_system" "cpd_efs" {
+   creation_token = "cpd_efs"
+   performance_mode = "generalPurpose"
+   throughput_mode = "provisioned"
+   provisioned_throughput_in_mibps = "250"
+   encrypted = "true"
+   #lifecycle_policy {
+   #   transition_to_ia = "AFTER_30_DAYS"
+   #}
+   tags = {
+     Name = var.cluster_name
+   }
+ }
 
-# resource "aws_efs_mount_target" "cpd-efs-mt" {
-#    count = var.az == "multi_zone" ? 3 : 1
-#    file_system_id  = aws_efs_file_system.cpd_efs.id
-#    subnet_id = var.subnet_ids[count.index]
-#    security_groups = [aws_security_group.efs_sg.id]
-#  }
+resource "aws_efs_mount_target" "cpd-efs-mt" {
+   count = var.az == "multi_zone" ? 3 : 1
+   file_system_id  = aws_efs_file_system.cpd_efs.id
+   subnet_id = var.subnet_ids[count.index]
+   security_groups = [aws_security_group.efs_sg.id]
+ }
 
-# resource "aws_security_group" "efs_sg" {
-#    name = "efs_sg"
-#    description= "Allos inbound efs traffic from ec2"
-#    vpc_id = data.aws_vpc.cpd_vpc.id
+resource "aws_security_group" "efs_sg" {
+   name = "efs_sg"
+   description= "Allos inbound efs traffic from ec2"
+   vpc_id = data.aws_vpc.cpd_vpc.id
 
-#    ingress {
-#      cidr_blocks = [data.aws_vpc.cpd_vpc.cidr_block]
-#      from_port = 2049
-#      to_port = 2049 
-#      protocol = "tcp"
-#    }     
+   ingress {
+     cidr_blocks = [data.aws_vpc.cpd_vpc.cidr_block]
+     from_port = 2049
+     to_port = 2049 
+     protocol = "tcp"
+   }     
 
-#    tags = {
-#      Name = var.cluster_name
-#    }
-#  }
+   tags = {
+     Name = var.cluster_name
+   }
+ }
 
-# resource "aws_iam_policy" "efs_policy" {
-#   name        = "aws_efs_policy"
-#   description = "EFS policy"
+resource "aws_iam_policy" "efs_policy" {
+  name        = "aws_efs_policy"
+  description = "EFS policy"
 
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": [
-#         "elasticfilesystem:DescribeAccessPoints",
-#         "elasticfilesystem:DescribeFileSystems"
-#       ],
-#       "Resource": "*"
-#     },
-#     {
-#       "Effect": "Allow",
-#       "Action": [
-#         "elasticfilesystem:CreateAccessPoint"
-#       ],
-#       "Resource": "*",
-#       "Condition": {
-#         "StringLike": {
-#           "aws:RequestTag/efs.csi.aws.com/cluster": "true"
-#         }
-#       }
-#     },
-#     {
-#       "Effect": "Allow",
-#       "Action": "elasticfilesystem:DeleteAccessPoint",
-#       "Resource": "*",
-#       "Condition": {
-#         "StringEquals": {
-#           "aws:ResourceTag/efs.csi.aws.com/cluster": "true"
-#         }
-#       }
-#     }
-#   ]
-# }
-# EOF
-# }
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "elasticfilesystem:DescribeAccessPoints",
+        "elasticfilesystem:DescribeFileSystems"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "elasticfilesystem:CreateAccessPoint"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "aws:RequestTag/efs.csi.aws.com/cluster": "true"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": "elasticfilesystem:DeleteAccessPoint",
+      "Resource": "*",
+      "Condition": {
+        "StringEquals": {
+          "aws:ResourceTag/efs.csi.aws.com/cluster": "true"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
 
-# data "aws_iam_roles" "efs_worker_roles" {
-#   name_regex = "^${var.cluster_name}.*worker-role$"
-# }
+data "aws_iam_roles" "efs_worker_roles" {
+  name_regex = "^${var.cluster_name}.*worker-role$"
+}
 
-# #data "aws_iam_role" "efs_worker_role" {
-# ##  name = "an_example_role_name"
-# #}
+#data "aws_iam_role" "efs_worker_role" {
+##  name = "an_example_role_name"
+#}
 
-# resource "aws_iam_role_policy_attachment" "efs-policy-attach" {
-#   role       = tolist(data.aws_iam_roles.efs_worker_roles.names)[0]
-#   policy_arn = aws_iam_policy.efs_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "efs-policy-attach" {
+  role       = tolist(data.aws_iam_roles.efs_worker_roles.names)[0]
+  policy_arn = aws_iam_policy.efs_policy.arn
+}
 
-# resource "null_resource" "login_cluster" {
-#   triggers = {
-#     openshift_api       = var.openshift_api
-#     openshift_username  = var.openshift_username
-#     openshift_password  = var.openshift_password
-#     openshift_token     = var.openshift_token
-#     login_string        = var.login_cmd
-#   }
-#   provisioner "local-exec" {
-#     command = <<EOF
+resource "null_resource" "nfs_subdir_provisioner_setup" {
+  triggers = { 
+         login_cmd           =  var.login_cmd
+         openshift_username      = regex("username (.*) --password", "${var.login_cmd}")[0]
+         openshift_api        =  regex("login (.*) --username","${var.login_cmd}")[0]
+         openshift_password      = regex("--password (.*)","${var.login_cmd}")[0]
+         cluster_type        = "managed"
+         file_system_id  = aws_efs_file_system.cpd_efs.id
+  }
+  provisioner "local-exec" {
+    command = <<EOF
+    bash efs/setup-nfs.sh ${self.triggers.openshift_api} '${self.triggers.openshift_username}' '${self.triggers.openshift_password}' '${self.triggers.file_system_id}'
+EOF
+  }
+  depends_on = [
+    resource.aws_efs_mount_target.cpd-efs-mt
+  ]
+}
+
 # ${self.triggers.login_string} || oc login ${self.triggers.openshift_api} -u '${self.triggers.openshift_username}' -p '${self.triggers.openshift_password}' --insecure-skip-tls-verify=true || oc login --server='${self.triggers.openshift_api}' --token='${self.triggers.openshift_token}'
-# EOF
-#   }
-#   depends_on = [
-#     resource.aws_efs_mount_target.cpd-efs-mt
-#   ]
-# }
 
 # resource "null_resource" "configure_efs" {
 #   triggers = {
@@ -145,12 +148,12 @@ resource "null_resource" "cpd_efs" {
 #   }
 #   provisioner "local-exec" {
 #     command = <<EOF
-# echo "Creating SC"
-# oc create -f ${self.triggers.installer_workspace}/efs_sc.yaml
-# oc create -f ${self.triggers.installer_workspace}/efs_sc_wkc.yaml
-# echo "Creating test pvc"
-# oc create -f ${self.triggers.installer_workspace}/efs_test_pvc.yaml
-# sleep 60
+# # echo "Creating SC"
+# # oc create -f ${self.triggers.installer_workspace}/efs_sc.yaml
+# # oc create -f ${self.triggers.installer_workspace}/efs_sc_wkc.yaml
+# # echo "Creating test pvc"
+# # oc create -f ${self.triggers.installer_workspace}/efs_test_pvc.yaml
+# # sleep 60
 
 # EOF
 #   }
