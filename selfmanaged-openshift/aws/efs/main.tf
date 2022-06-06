e4b8e5550327f98727b7f56e82c6bb1e9efa46e3
vamshi cholleti <vamshicholleti@vamshis-MBP.alphionsee.in>
2022-06-07 00:53:42 +0530

    fixed EFS setup code v1

--- selfmanaged-openshift/aws/efs/main.tf
+++ selfmanaged-openshift/aws/efs/main.tf
@@ -41,34 +41,39 @@ resource "aws_efs_file_system" "cpd_efs" {
    }
  }
 
+data "aws_security_group" "aws_worker_sg" {
+   tags = {
+    Name   = "${var.cluster_name}-*-worker-sg"
+  }
+}
+
 resource "aws_efs_mount_target" "cpd-efs-mt" {
    count = var.az == "multi_zone" ? 3 : 1
    file_system_id  = aws_efs_file_system.cpd_efs.id
    subnet_id = var.subnet_ids[count.index]
-   security_groups = [aws_security_group.efs_sg.id]
+   security_groups = [data.aws_security_group.aws_worker_sg.id]
 
    depends_on = [
     aws_efs_file_system.cpd_efs,
-    aws_security_group.efs_sg,
   ]   
  }
 
-resource "aws_security_group" "efs_sg" {
-   name = "efs_sg"
-   description= "Allos inbound efs traffic from ec2"
-   vpc_id = data.aws_vpc.cpd_vpc.id
+# resource "aws_security_group" "efs_sg" {
+#    name = "efs_sg"
+#    description= "Allos inbound efs traffic from ec2"
+#    vpc_id = data.aws_vpc.cpd_vpc.id
 
-   ingress {
-     cidr_blocks = [data.aws_vpc.cpd_vpc.cidr_block]
-     from_port = 2049
-     to_port = 2049 
-     protocol = "tcp"
-   }     
+#    ingress {
+#      cidr_blocks = [data.aws_vpc.cpd_vpc.cidr_block]
+#      from_port = 2049
+#      to_port = 2049 
+#      protocol = "tcp"
+#    }     
 
-   tags = {
-     Name = var.cluster_name
-   }
- }
+#    tags = {
+#      Name = var.cluster_name
+#    }
+#  }
 
 resource "aws_iam_policy" "efs_policy" {
   name        = "aws_efs_policy"
@@ -117,10 +122,6 @@ data "aws_iam_roles" "efs_worker_roles" {
   name_regex = "^${var.cluster_name}.*worker-role$"
 }
 
-#data "aws_iam_role" "efs_worker_role" {
-##  name = "an_example_role_name"
-#}
-
 resource "aws_iam_role_policy_attachment" "efs-policy-attach" {
   role       = tolist(data.aws_iam_roles.efs_worker_roles.names)[0]
   policy_arn = aws_iam_policy.efs_policy.arn
@@ -146,51 +147,6 @@ EOF
     aws_iam_role_policy_attachment.efs-policy-attach,
   ]
 }
-# ${self.triggers.login_string} || oc login ${self.triggers.openshift_api} -u '${self.triggers.openshift_username}' -p '${self.triggers.openshift_password}' --insecure-skip-tls-verify=true || oc login --server='${self.triggers.openshift_api}' --token='${self.triggers.openshift_token}'
-
-# resource "null_resource" "configure_efs" {
-#   triggers = {
-#     installer_workspace = local.installer_workspace
-#   }
-#   provisioner "local-exec" {
-#     command = <<EOF
-# echo "Creating SC"
-# oc create -f ${self.triggers.installer_workspace}/efs_sc.yaml
-# oc create -f ${self.triggers.installer_workspace}/efs_sc_wkc.yaml
-# echo "Creating test pvc"
-# oc create -f ${self.triggers.installer_workspace}/efs_test_pvc.yaml
-# sleep 60
-
-# EOF
-#   }
-#   depends_on = [
-#     null_resource.login_cluster
-#   ]
-# }
-
-# resource "local_file" "efs_test_pvc_yaml" {
-#   content  = data.template_file.efs_test_pvc.rendered
-#   filename = "${local.installer_workspace}/efs_test_pvc.yaml"
-#   depends_on = [
-#     resource.aws_efs_mount_target.cpd-efs-mt
-#   ]
-# }
-
-# resource "local_file" "efs_sc_yaml" {
-#   content  = data.template_file.efs_sc.rendered
-#   filename = "${local.installer_workspace}/efs_sc.yaml"
-#   depends_on = [
-#     resource.aws_efs_mount_target.cpd-efs-mt
-#   ]
-# }
-
-# resource "local_file" "efs_sc_wkc_yaml" {
-#   content  = data.template_file.efs_sc_wkc.rendered
-#   filename = "${local.installer_workspace}/efs_sc_wkc.yaml"
-#   depends_on = [
-#     resource.aws_efs_mount_target.cpd-efs-mt
-#   ]
-# }
 
 locals {
   installer_workspace = var.installer_workspace
