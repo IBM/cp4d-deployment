4921419e04ac8e59bf125657d52de84a342efb8f
shankarpentyala07 <shankarpentyala@gmail.com>
2022-06-11 15:46:13 -0700

    Add storage vendor for portworx storage

--- common-modules/cpd/scripts/apply-cr.sh
+++ common-modules/cpd/scripts/apply-cr.sh
@@ -5,8 +5,8 @@ CPD_RELEASE=$2
 COMPONENTS=$3
 NAMESPACE=$4
 SC_VENDOR=$5
-FILE_SC=$6
-BLOCK_SC=$7
+FILE_SC=$5
+BLOCK_SC=$6
 if [[ "$SC_VENDOR" != "portworx" ]]
 then
    $INSTALLER_PATH/cpd-cli manage apply-cr --release=$CPD_RELEASE --components=$COMPONENTS  --license_acceptance=true --cpd_instance_ns=$NAMESPACE --file_storage_class=$FILE_SC --block_storage_class=$BLOCK_SC
