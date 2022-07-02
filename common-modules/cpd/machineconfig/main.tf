759aa5acef3ce3f6418400b77fe404f1f72e5099
Shankar Pentyala <shankarpentyala@Shankars-MacBook-Pro.local>
2022-07-01 17:34:49 -0700

    rosa uncomment label worker mcp for an issue

--- common-modules/cpd/machineconfig/main.tf
+++ common-modules/cpd/machineconfig/main.tf
@@ -66,11 +66,7 @@ resource "null_resource" "patch_config_managed" {
 echo "Patch configuration self"
 oc patch kubeletconfig custom-kubelet --type='json' -p='[{"op": "remove", "path": "/spec/machineConfigPoolSelector/matchLabels"}]'
 oc patch kubeletconfig custom-kubelet --type merge -p '{"spec":{"machineConfigPoolSelector":{"matchLabels":{"pools.operator.machineconfiguration.openshift.io/master":""}}}}'
-#Rosa issue - so commenting for temp fix -Error from server (Prevented from accessing Red Hat managed resources. 
-#This is in an effort to prevent harmful actions that may cause unintended consequences or affect the stability of the cluster.
-#admission webhook "regular-user-validation.managed.openshift.io" denied the request: Prevented from accessing Red Hat managed resources. 
-#This is in an effort to prevent harmful actions that may cause unintended consequences or affect the stability of the cluster
-#oc label machineconfigpool.machineconfiguration.openshift.io worker db2u-kubelet=sysctl --overwrite
+oc label machineconfigpool.machineconfiguration.openshift.io worker db2u-kubelet=sysctl --overwrite
 EOF
   }
   depends_on = [
